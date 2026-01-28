d7e7db70233ad68bd36153ecfc118a562180b5df
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-01-28 14:20:44 -0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/brightness/store/BrightnessStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.brightness.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightnessStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightnessStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightness/store/BrightnessStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightness.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightnessStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
