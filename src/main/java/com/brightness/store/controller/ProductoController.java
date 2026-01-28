94c5daa44b277872d921693de8951598cbfcb3da
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-01-28 17:39:18 -0300

    add endpoint to get broduct by id with 404 handling

--- src/main/java/com/brightness/store/controller/ProductoController.java
+++ src/main/java/com/brightness/store/controller/ProductoController.java
@@ -3,13 +3,14 @@ package com.brightness.store.controller;
 import com.brightness.store.entity.Producto;
 import com.brightness.store.repository.ProductoRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 
 import java.util.List;
 
 @RestController
 public class ProductoController{
-  
+
   private final ProductoRepository productoRepository;
 
   public ProductoController(ProductoRepository productoRepository){
@@ -19,6 +20,14 @@ public class ProductoController{
   @GetMapping("/productos")
   public List<Producto> obtenerProductos(){
     return productoRepository.findAll();
+  }
+
+  @GetMapping("/productos/{id}")
+  public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Long id) {
+
+    return productoRepository.findById(id)
+            .map(producto -> ResponseEntity.ok(producto))
+            .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping("/productos")
